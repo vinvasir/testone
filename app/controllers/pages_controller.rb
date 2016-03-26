58cce159c16146b41a5b2946fa8874fbd107c4e2
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-03-26 17:37:01 +0000

    Made the create action add a new review

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
   def new
     @page=Page.new
   end
+
+  def create
+    Page.create(page_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title, :description, :channel)
+  end
 end
